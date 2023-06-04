9c8bc3c09bf393458308163c44b745df6490e858
romeomoreno <104795749+romeomoreno@users.noreply.github.com>
2023-06-04 18:03:44 -0300

    Actualización de serializer y url

--- back-end/educando-back/educando_back/urls.py
+++ back-end/educando-back/educando_back/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for educandoprueba12 project.
+URL configuration for educando project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,26 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.urls import path, include
-from rest_framework import routers
-from educando_ecommerce import views
 from rest_framework.documentation import include_docs_urls
 from django.contrib import admin
 
-router = routers.DefaultRouter()
-
-router.register(r'usuarios', views.UsuarioViewSet)
-
-router.register(r'categorias', views.CategoriaViewSet)
-router.register(r'cursos', views.CursoViewSet)
-router.register(r'miscursos', views.MisCursoViewSet)    
-router.register(r'carritos', views.CarritoViewSet)
-router.register(r'foros', views.ForoViewSet)
-router.register(r'contactos', views.ContactoViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-     path('docs/', include_docs_urls(title='appEducando')),
+    path('', include('educando_eco.urls')),
+    path('docs/', include_docs_urls(title='Educando')),
     path('admin/', admin.site.urls),
-    ]
+]
--- back-end/educando-back/educando_ecommerce/serializer.py
+++ back-end/educando-back/educando_ecommerce/serializer.py
@@ -2,36 +2,71 @@ from  rest_framework import serializers
 
 from .models import Rol, Usuario, Categoria,Curso, MisCurso, Carrito, Foro, Contacto
 
-
 class RolSerializer(serializers.ModelSerializer):    
     class Meta:
         model = Rol
         fields = '__all__'
 
-class UsuarioSerializer(serializers.ModelSerializer):    
+#===========================================================================================================================================================================
+class UsuarioSerializer(serializers.Serializer):
+    # Se definen los campos del serializer
+    nombre = serializers.CharField(required=True)
+    apellido = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True)
+    id_rol_id = serializers.IntegerField(required=True)
+
     class Meta:
         model = Usuario
-        fields = '__all__'
+        fields = ['nombre', 'apellido', 'email', 'password','id_rol_id']
+        extra_kwargs = {'password': {'write_only': True}}
+        #El campo password tiene la configuración adicional write_only=True, lo que significa que solo se utilizará para la creación de un nuevo usuario y no se incluirá en las respuestas de serialización
+
+    def create(self, validated_data):
+        # Se extrae el campo 'password' del diccionario de datos validados
+        password = validated_data.pop('password')
+        # Se crea un nuevo usuario utilizando el método create_user de la clase Usuario
+        usuario = Usuario.objects.create_user(password=password, **validated_data)
+        # Se retorna el usuario creado
+        return usuario
+    
+#===========================================================================================================================================================================
+
 class CategoriaSerializer(serializers.ModelSerializer):   
     class Meta:
         model = Categoria
         fields = '__all__'
+
 class CursoSerializer(serializers.ModelSerializer): 
     class Meta:
         model = Curso
         fields = '__all__'
-class MisCursoSerializer(serializers.ModelSerializer):  
+
+#===========================================================================================================================================================================
+
+class MisCursoSerializer(serializers.ModelSerializer):
+    id_curso = serializers.PrimaryKeyRelatedField(queryset=Curso.objects.all())  # Campo para el ID del curso
+
+    # Agregar campo de descripción
+    nombre_curso = serializers.CharField(source='id_curso.nombre_curso', read_only=True)
+    descripcion_curso = serializers.CharField(source='id_curso.descripcion', read_only=True)
+    
     class Meta:
         model = MisCurso
-        fields = '__all__'
+        fields = ['id_mis_curso', 'id_usuario', 'id_curso', 'nombre_curso', 'descripcion_curso']
+
+
+#===========================================================================================================================================================================
 class CarritoSerializer(serializers.ModelSerializer):   
     class Meta:
         model = Carrito
         fields = '__all__'
+
 class ForoSerializer(serializers.ModelSerializer):   
     class Meta:
         model = Foro
         fields = '__all__'
+
 class ContactoSerializer(serializers.ModelSerializer):  
     class Meta:
         model = Contacto
